9a80dd60a62f2f721fa279a9c9c2f34d6f46d887
Mike Altonji <mikealtonji@gmail.com>
2023-09-24 01:51:43 -0400

    Dummy logic for using asset_pair data
    TODO: `random_logic` isn't seeing any updated data. figure out why.

--- src/kraken_ws.rs
+++ src/kraken_ws.rs
@@ -4,6 +4,7 @@ use serde_json::{json, Value};
 use std::collections::HashMap;
 use futures_util::StreamExt;
 use futures_util::sink::SinkExt;
+use std::sync::{Arc, Mutex};
 
 pub async fn asset_pairs_to_pull() -> Result<HashMap<String, (String, String)>, Box<dyn std::error::Error>> {
     // Fetch the list of all asset pairs
@@ -13,19 +14,23 @@ pub async fn asset_pairs_to_pull() -> Result<HashMap<String, (String, String)>, 
     let data: Value = serde_json::from_str(&text)?;
     let mut pair_to_assets = HashMap::new();
 
+    let desired_pairs = ["XBT/USD", "EUR/CAD"];  // For testing, just grab 2
+
     if let Some(pairs) = data["result"].as_object() {
         for (_pair, details) in pairs {
             let wsname: String = details["wsname"].as_str().unwrap_or("").to_string();
-            let base = details["base"].as_str().unwrap_or("").to_string();
-            let quote = details["quote"].as_str().unwrap_or("").to_string();
-            pair_to_assets.insert(wsname, (base, quote));
+            if desired_pairs.contains(&wsname.as_str()) {
+                let base = details["base"].as_str().unwrap_or("").to_string();
+                let quote = details["quote"].as_str().unwrap_or("").to_string();
+                pair_to_assets.insert(wsname, (base, quote));
+            }
         }
     }
     Ok(pair_to_assets)
 }
 
 
-pub async fn fetch_kraken_data_ws(pair_to_assets: HashMap<String, (String, String)>) -> Result<HashMap<String, (f64, f64)>, Box<dyn std::error::Error>> {
+pub async fn fetch_kraken_data_ws(pair_to_assets: HashMap<String, (String, String)>, shared_asset_pairs: Arc<Mutex<HashMap<String, (f64, f64)>>>) -> Result<(), Box<dyn std::error::Error>> {
     let url = url::Url::parse("wss://ws.kraken.com").unwrap();
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     let (mut write, mut read) = ws_stream.split();
@@ -36,7 +41,6 @@ pub async fn fetch_kraken_data_ws(pair_to_assets: HashMap<String, (String, Strin
     });
     write.send(Message::Text(subscription_message.to_string())).await?;
 
-    let mut asset_pairs = HashMap::new();
     while let Some(msg) = read.next().await {
         match msg {
             Ok(Message::Text(text)) => {
@@ -47,12 +51,13 @@ pub async fn fetch_kraken_data_ws(pair_to_assets: HashMap<String, (String, Strin
                         if let Some(inner_array) = array[1].as_array() {
                             let bid = inner_array.get(0).and_then(|s| s.as_str()).and_then(|s| s.parse::<f64>().ok()).unwrap_or(0.0);
                             let ask = inner_array.get(1).and_then(|s| s.as_str()).and_then(|s| s.parse::<f64>().ok()).unwrap_or(0.0);
-                            asset_pairs.insert(pair.to_string(), (bid, ask));
-                            println!("Pair: {} | bid={} ask={}", pair, bid, ask);
+
+                            let mut locked_pairs = shared_asset_pairs.lock().unwrap();
+                            locked_pairs.insert(pair.to_string(), (bid, ask));
+                            // println!("Pair: {} | bid={} ask={}", pair, bid, ask);
                         }
                     }
                 }
-                
             },
             Err(e) => {
                 println!("Error during websocket communication: {:?}", e);
@@ -60,5 +65,5 @@ pub async fn fetch_kraken_data_ws(pair_to_assets: HashMap<String, (String, Strin
             _ => {} // Handle other message types if needed.
         }
     }
-    Ok(asset_pairs)
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
 mod kraken_ws;
+mod random_logic;
 
 #[tokio::main]
 async fn main() {
     let pair_to_assets = kraken_ws::asset_pairs_to_pull().await.expect("Failed to get asset pairs");
-    let asset_pairs = kraken_ws::fetch_kraken_data_ws(pair_to_assets.clone()).await.expect("Failed to fetch data");
+    let shared_asset_pairs = Arc::new(Mutex::new(HashMap::new()));
 
-    println!("Asset Pairs:");
-    for (pair, (bid, ask)) in &asset_pairs {
-        println!("{}: Bid: {}, Ask: {}", pair, bid, ask);
-    }
+    let fetch_handle = {
+        let shared_asset_pairs_clone = shared_asset_pairs.clone();
+        tokio::spawn(async move {
+            kraken_ws::fetch_kraken_data_ws(pair_to_assets.clone(), shared_asset_pairs_clone).await.expect("Failed to fetch data");
+        })
+    };
+    
+    let monitor_handle = {
+        let shared_asset_pairs_clone = shared_asset_pairs.clone();
+        tokio::spawn(async move {
+            random_logic::monitor_btc_usd(shared_asset_pairs_clone).await;
+        })
+    };
 
-    println!("\nPair to Assets:");
-    for (pair, (base, quote)) in &pair_to_assets {
-        println!("{}: Base: {}, Quote: {}", pair, base, quote);
-    }
+    // Wait for both tasks to complete (this will likely never happen given the current logic)
+    let _ = tokio::try_join!(fetch_handle, monitor_handle);
 }
