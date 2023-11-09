fd6f9b40ee84c77bc41b3a311f484cedd5aa47de
Mike Altonji <mikealtonji@gmail.com>
2023-11-08 23:46:45 -0500

    Setup DB with script

---
+++ scripts/setup_influxdb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Be sure to run `chmod +x scripts/setup_influxdb.sh` before running this for the first time
+
+# Navigate to the parent directory where the .env file is located
+cd "$(dirname "$0")/.."
+
+# Source the environment variables from the .env file
+if [ -f .env ]; then
+  export $(egrep -v '^#' .env | xargs)
+fi
+
+# Create the InfluxDB database
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=CREATE DATABASE \"$DB_NAME\""
+
+# Create the admin user
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=CREATE USER \"$DB_ADMIN_USER\" WITH PASSWORD '$DB_ADMIN_PASSWORD' WITH ALL PRIVILEGES"
+
+# Create the regular user, and grant read/write permissions to the regular user on the specific database
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=CREATE USER \"$DB_USER\" WITH PASSWORD '$DB_PASSWORD'"
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=GRANT READ ON \"$DB_NAME\" TO \"$DB_USER\""
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=GRANT WRITE ON \"$DB_NAME\" TO \"$DB_USER\""
+
+# Create the retention policy
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=CREATE RETENTION POLICY \"$RP_NAME\" ON \"$DB_NAME\" DURATION $RP_DURATION REPLICATION 1"
+
+# Create the continuous query
+curl -XPOST "http://$INFLUXDB_HOST:$INFLUXDB_PORT/query" --data-urlencode "q=CREATE CONTINUOUS QUERY \"cq_spread_latency_aggregates\" ON \"$DB_NAME\" BEGIN SELECT count(\"latency\") AS count_values, mean(\"latency\") AS mean_latency, min(\"latency\") AS min_latency, max(\"latency\") AS max_latency, percentile(\"latency\", 1) AS p01_latency, percentile(\"latency\", 10) AS p10_latency, percentile(\"latency\", 25) AS p25_latency, percentile(\"latency\", 50) AS p50_latency, percentile(\"latency\", 75) AS p75_latency, percentile(\"latency\", 90) AS p90_latency, percentile(\"latency\", 99) AS p99_latency INTO \"spread_latency_aggregates\" FROM \"$RP_NAME\".\"spread_latency\" GROUP BY time(5m), pair END"
+
+# Output the results
+echo "End of script."
