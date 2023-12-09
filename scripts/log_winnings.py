b580ec6efb6d93b3c9b8300aea47ffe4de18d037
Mike Altonji <mikealtonji@gmail.com>
2023-12-09 14:01:21 -0500

    Rewrote how we log winnings in Influx

--- scripts/log_winnings.py
+++ scripts/log_winnings.py
@@ -1,23 +1,17 @@
 """
-Returns the cumulative reward up to this point in time.
-In InfluxQL, we can look at the delta between entries with:
-```
-USE arbitrage;
-SELECT difference("winnings_first") as winnings_first, difference("winnings_max") as winnings_max
-FROM winnings
-GROUP BY available_cash, min_roi
-```
+Returns the reward per hour for the prior 3 days.
+USD and EUR are considered separately, avoiding unit conversions.
+In Influx, will OVERWRITE the prior 3 days of values per hour, by design. 
+Allows for overwriting `max` values for arbitrage opportunities spanning multiple hours.
+In InfluxQL, can create a cumulative chart as well.
 """
 
 from dotenv import load_dotenv
 from influxdb import DataFrameClient
 import os
 import pandas as pd
-import requests
-import time
 
 # InfluxDB Configs
-start_time = time.time()
 load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
@@ -28,6 +22,8 @@ OUTPUT_TABLE = "winnings"
 # Implementation configurations
 AVAILABLE_CASH_LIST = [1_000, 2_500, 5_000, 10_000, 1_000_000]
 MIN_ROI_LIST = [0, 0.0025]
+MIN_VOLUME_LIST = [0, 10]
+STARTER_LIST = ["USD", "EUR"]
 MIN_TIME_SINCE_LAST_ARBITRAGE = "1H"
 
 # Connect to InfluxDB
@@ -43,10 +39,12 @@ except Exception as e:
 query = f"""
 SELECT path, limiting_volume, ending_volume, volume_units
 FROM {INPUT_TABLE}
-WHERE volume_units = 'USD' OR volume_units = 'EUR'
+WHERE 
+    time > now() - 3d 
+    AND (volume_units = 'USD' OR volume_units = 'EUR')
 """
 try:
-    df = client.query(query=query, epoch="s")
+    df = client.query(query=query)
     df = df[INPUT_TABLE]
     if not isinstance(df, pd.DataFrame):
         raise ValueError(f"The fetched data is not a DataFrame: {type(df)}")
@@ -55,6 +53,8 @@ except Exception as e:
     print("Error: ", e)
 
 # Compute winnings
+df.reset_index(drop=False, inplace=True)
+df.rename(columns={"index": "timestamp"}, inplace=True)
 df["winnings"] = df["ending_volume"] - df["limiting_volume"]
 df["roi"] = df["ending_volume"] / df["limiting_volume"] - 1
 
@@ -66,52 +66,48 @@ def limit_winnings_by_available_cash(row, limiter):
     return row
 
 def calculate_time_diff(df):
-    df = df.sort_index()  # Ensure the data is sorted by time
-    df['time_diff'] = df.index.to_series().diff()  # Calculate the time difference between rows
+    df = df.sort_values(by=["timestamp"])  # Ensure the data is sorted by time
+    df['time_diff'] = df["timestamp"].diff()  # Calculate the time difference between rows
     df['group'] = (df['time_diff'] > pd.Timedelta(MIN_TIME_SINCE_LAST_ARBITRAGE)).cumsum()
     return df
 
-def convert_to_usd(df):
-    non_usd_rows = df[df['volume_units'] != 'USD']
-    for volume_unit in non_usd_rows['volume_units'].unique():
-        try:
-            response = requests.get(f'https://api.exchangerate-api.com/v4/latest/{volume_unit}')
-            data = response.json()
-            conversion_rate = data['rates']['USD']
-            df.loc[df['volume_units'] == volume_unit, 'limiting_volume'] *= conversion_rate
-            df.loc[df['volume_units'] == volume_unit, 'ending_volume'] *= conversion_rate
-        except Exception as e:
-            print(f"Failed to fetch conversion rate for {volume_unit}/USD.")
-            print("Error: ", e)
-    return df
+for starter in STARTER_LIST:
+    df2 = df[df["volume_units"] == starter]
+    for min_vol in MIN_VOLUME_LIST:
+        for min_roi in MIN_ROI_LIST:
+            for available_cash in AVAILABLE_CASH_LIST:
+                tmp = df2.copy()
 
-# Convert all units to USD.
-# NOTE: If there's a large swing in conversion, the diff in InfluxQL will swing as well.
-df = convert_to_usd(df)
+                # Group arbitrage rows of the same path until there's a break of `MIN_TIME_SINCE_LAST_ARBITRAGE` time between instances
+                grouped = tmp.groupby('path').apply(calculate_time_diff).reset_index(drop=True)
 
-for min_roi in MIN_ROI_LIST:
-    for available_cash in AVAILABLE_CASH_LIST:
-        tmp = df.copy()
+                # Limit based on available cash, and filter out low ROI opportunities
+                df_limited = grouped.apply(lambda row: limit_winnings_by_available_cash(row, available_cash), axis=1)
+                df_high_roi = df_limited[df_limited["roi"] > min_roi]
+                df_high_roi = df_high_roi[df_high_roi["limiting_volume"] > min_vol]  # Do after calculating time diff
 
-        # Group arbitrage rows of the same path until there's a break of `MIN_TIME_SINCE_LAST_ARBITRAGE` time between instances
-        grouped = tmp.groupby('path').apply(calculate_time_diff).reset_index(drop=True)
-        
-        # Limit based on available cash, and filter out low ROI opportunities
-        df_limited = grouped.apply(lambda row: limit_winnings_by_available_cash(row, available_cash), axis=1)
-        df_high_roi = df_limited[df_limited["roi"] > min_roi]        
+                # Grab the first, max winnings per opportunity group
+                df_first = df_high_roi.groupby(['group', 'path']).first()[['winnings', 'timestamp']]
+                idx_max = df_high_roi.groupby(['group', 'path'])['winnings'].idxmax()
+                df_max = df_high_roi.loc[idx_max, ['winnings', 'timestamp']]
 
-        # Grab the first, max winnings per opportunity group
-        df_first = df_high_roi.groupby(['group', 'path']).first()['winnings']
-        df_max = df_high_roi.groupby(['group', 'path']).max()['winnings']
+                # Resample the data to hourly intervals and sum the winnings
+                df_first.set_index('timestamp', inplace=True)
+                df_max.set_index('timestamp', inplace=True)
+                df_first_hourly = df_first.resample('H').sum()
+                df_max_hourly = df_max.resample('H').sum()
 
-        # Write these results to a new influxdb measurement
-        point = pd.DataFrame(index=pd.DatetimeIndex([pd.Timestamp(start_time, unit='s')]),
-                                    data={
-                                        "available_cash": [available_cash],
-                                        "min_roi": [min_roi],
-                                        "winnings_first": [df_first.sum()], 
-                                        "winnings_max": [df_max.sum()]
-                                    })
-        client.write_points(point, OUTPUT_TABLE, tag_columns=["available_cash", "min_roi"])
+                # Create a new DataFrame with 'first' and 'max' columns
+                df_hourly_winnings = pd.DataFrame({
+                    'first': df_first_hourly['winnings'],
+                    'max': df_max_hourly['winnings']
+                })
+                df_hourly_winnings["starter"] = starter
+                df_hourly_winnings["min_volume"] = min_vol
+                df_hourly_winnings["available_cash"] = available_cash
+                df_hourly_winnings["min_roi"] = min_roi
+
+                # Write these results to InfluxDB
+                client.write_points(df_hourly_winnings, OUTPUT_TABLE, tag_columns=["starter", "min_volume", "available_cash", "min_roi"])
 
 client.close()
