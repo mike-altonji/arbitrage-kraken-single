da3d94527e6a2b01e1cbc15732e5696f0e398253
Mike Altonji <mikealtonji@gmail.com>
2025-12-23 10:24:21 -0500

    Pull PairData into evaluation

--- src/evaluate_arbitrage.rs
+++ src/evaluate_arbitrage.rs
@@ -1,9 +1,20 @@
 use crate::structs::PairDataVec;
 
-pub fn evaluate_arbitrage(pair_data_vec: &mut PairDataVec, idx: usize) {
-    let pair_data = pair_data_vec.get(idx).unwrap();
-    let bid_price = pair_data.bid_price;
-    let ask_price = pair_data.ask_price;
-    let bid_volume = pair_data.bid_volume;
-    let ask_volume = pair_data.ask_volume;
+pub fn evaluate_arbitrage(pair_data_vec: &PairDataVec, idx: usize) {
+    let usd_pair = pair_data_vec.get(idx - (idx % 2));
+    let eur_pair = pair_data_vec.get(idx + 1 - (idx % 2));
+    let usd_stable_pair = pair_data_vec.get(0);
+    let eur_stable_pair = pair_data_vec.get(1);
+    if usd_pair.is_none()
+        || eur_pair.is_none()
+        || usd_stable_pair.is_none()
+        || eur_stable_pair.is_none()
+    {
+        log::error!("Failed to get pair for index {} or stablecoin", idx);
+        return;
+    }
+    let usd_pair = usd_pair.unwrap();
+    let eur_pair = eur_pair.unwrap();
+    let usd_stable_pair = usd_stable_pair.unwrap();
+    let eur_stable_pair = eur_stable_pair.unwrap();
 }
